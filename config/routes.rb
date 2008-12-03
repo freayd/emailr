c2ee949051b4b291a69311b3e1ced5934c89c84c
fred <fred.macias@gmail.com>
2008-12-03 14:35:36 +0100

    Rajout du contrôleur 'Home'. Rajout d'un layout et d'un style pour le frontend.

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => 'sessions', :conditions => { :method => :get }
+  map.root :controller => 'home', :conditions => { :method => :get }
 
   # See how all your routes lay out with "rake routes"
 
