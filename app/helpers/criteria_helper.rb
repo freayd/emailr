9296cb179b9ecbe5e5ef44e0fff43716143e2a34
fred <fred.macias@gmail.com>
2008-12-14 12:38:46 +0100

    Amélioration des conditions sur les critères: la condition dépend maintenant du champ visé par le critère.

--- app/helpers/criteria_helper.rb
+++ app/helpers/criteria_helper.rb
@@ -2,7 +2,7 @@ module CriteriaHelper
   def field_options_for_select
     Criterion.fields.collect { |field| [ t("criterion.field.#{field}"), field ] }
   end
-  def condition_options_for_select
-    Criterion.conditions.collect { |condition| [ t("criterion.condition.#{condition}"), condition ] }
+  def condition_options_for_select(field)
+    Criterion.conditions_for_field(field).collect { |condition| [ t("criterion.condition.#{condition}"), condition ] }
   end
 end
--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -7,8 +7,24 @@ class Criterion < ActiveRecord::Base
   def self.conditions
     @@conditions ||= %w( less_than less_than_or_equal_to equal_to not_equal_to greater_than_or_equal_to greater_than )
   end
+  def self.conditions_for_field(field)
+    return unless fields.include?(field)
 
-  validates_inclusion_of :field,     :in => self.fields
-  validates_inclusion_of :condition, :in => self.conditions
-  validates_presence_of  :value
+    case Subscriber.columns_hash[field].type
+      when :integer, :float, :decimal,
+           :datetime, :date, :timestamp, :time
+        then @@number_conditions   ||= %w( less_than less_than_or_equal_to equal_to not_equal_to greater_than_or_equal_to greater_than )
+      when :text, :string
+        then @@text_conditions     ||= %w( matches_exactly matches_regular_expression contains does_not_contain starts_with ends_with )
+      when :binary, :boolean
+        then @@boolean_conditions  ||= %w( equal_to not_equal_to )
+    end
+  end
+
+  validates_inclusion_of :field, :in => self.fields
+  validates_presence_of  :condition, :value
+
+  def validate
+    errors.add('condition', I18n.t('criterion.condition.invalid_for_field')) unless self.class.conditions_for_field(field).include?(condition)
+  end
 end
