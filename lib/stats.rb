6112a2674d10063019a5a6940d64873b8b392154
fred <fred.macias@gmail.com>
2008-12-18 17:37:21 +0100

    Calcul des décimales pour les statistiques.

--- lib/stats.rb
+++ lib/stats.rb
@@ -4,7 +4,7 @@ module Stats
   end
   def compute_delivery_failure_percent
     with_failure = current_customer.deliveries.count(:conditions => "failure = 't'")
-    @delivery_failure_percent ||= (with_failure * 100) / deliveries_count rescue nil
+    @delivery_failure_percent ||= (with_failure * 100).to_f / deliveries_count rescue nil
   end
   def compute_readed_percent
     readed = current_customer.deliveries.count('distinct(deliveries.id)',
@@ -12,6 +12,6 @@ module Stats
                                                          'INNER JOIN visitors      ON visitors.subscriber_id = deliveries.subscriber_id ' +
                                                          'INNER JOIN issues AS i1  ON emailing_logs.issue_id = i1.id ' +
                                                          'INNER JOIN issues AS i2  ON deliveries.issue_id    = i1.id')
-    @readed_percent ||= (readed * 100) / deliveries_count rescue nil
+    @readed_percent ||= (readed * 100).to_f / deliveries_count rescue nil
   end
 end
